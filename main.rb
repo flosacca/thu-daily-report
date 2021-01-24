69b3266d19d8cff8f269b03c9943b212f4abd566
flosacca <flosacca@gmail.com>
2021-01-24 19:09:17 +0800

    Finish most parts

--- main.rb
+++ main.rb
@@ -12,10 +12,10 @@ end
 class Reporter
   include Utils
 
-  ATTRIBUTES = {
-    service_id: 'b44e2daf-0ef6-4d11-a115-0eb0d397934f',
-    base_url: 'https://thos.tsinghua.edu.cn/fp/view?m=fp',
-    headers: {
+  ATTRIBUTES = Object.new.instance_eval do
+    @service_id = 'b44e2daf-0ef6-4d11-a115-0eb0d397934f'
+    @root_url = 'https://thos.tsinghua.edu.cn'
+    @headers = {
       'User-Agent' => [
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
         'AppleWebKit/537.36 (KHTML, like Gecko)',
@@ -25,20 +25,26 @@ class Reporter
       'Accept' => '*/*',
       'Accept-Encoding' => 'gzip, deflate',
       'Accept-Language' => 'zh-CN,zh;q=0.9'
-    },
-    cookies: []
-  }
+    }
+    @cookies = []
+    instance_variables.each_with_object({}) do |k, h|
+      h[k] = instance_variable_get(k)
+    end
+  end
 
-  attr_reader *ATTRIBUTES.keys
+  ATTRIBUTES.each_key do |k|
+    attr_reader k.to_s[1..-1]
+  end
 
   def initialize
     ATTRIBUTES.each do |k, v|
-      instance_variable_set("@#{k}", v)
+      instance_variable_set(k, v)
     end
   end
 
   def http
-    HTTP.use(:auto_inflate)
+    HTTP.timeout(10)
+      .use(:auto_inflate)
       .headers(headers)
       .cookies(cookies)
   end
@@ -46,11 +52,12 @@ class Reporter
   def login(username, password)
     # 从服务页面重定向到登录页面
     # 登录页面的地址与服务页面的子域名相关，因此通过重定向间接访问
-    res = http.follow.get(base_url)
+    res = http.follow.get(root_url + '/fp/')
     assert res.code == 200
 
     # 需要使用 GET 登录页面返回的 cookie 才能正确登录
-    # 诡异的是，如果指定使用过的此前返回的 cookie，也需要 GET 一次登录页面，否则会登录失败
+    # 诡异的是，即使指定使用过的此前返回的 cookie，也需要 GET
+    # 一次登录页面，否则会登录失败
     url = 'https://id.tsinghua.edu.cn/do/off/ui/auth/login/check'
     res = http.headers(referer: res.uri.to_s)
       .cookies(res.cookies)
@@ -71,11 +78,59 @@ class Reporter
   end
 
   def submit
-    # @service_url = "#{@base_url}#from=hall&serveID=#{@service_id}&act=fp/serveapply"
+    http = self.http.persistent(root_url)
+
+    res = http.post('/fp/fp/serveapply/getServeApply', json: {
+      serveID: service_id,
+      from: 'hall'
+    })
+    assert res.code == 200
+    res = JSON.parse(res.to_s)
+    form_id = res['formID']
+    proc_id = res['procID']
+    # privilege_id = res['privilegeID']
+
+    res = http.get('/fp/formParser', params: {
+      status: 'select',
+      formid: form_id
+    })
+    assert res.code == 200
+    doc = Nokogiri::HTML(res.to_s)
+    data = doc.at('#dcstr').text
+    puts data
+
+    # data = File.read('data.txt')
+    # data.gsub!('timestamp', '%.f' % [Time.now.to_f * 1000])
+
+    # res = http.headers({
+    #   'Content-Type' => 'text/plain;charset=UTF-8',
+    #   'Referer' => root_url + 'view?m=fp#' + URI.encode_www_form({
+    #     from: 'hall',
+    #     serveID: service_id,
+    #     act: 'fp/serveapply'
+    #   })
+    # }).post('/fp/formParser', {
+    #   params: {
+    #     status: 'update',
+    #     formid: form_id,
+    #     workflowAction: 'startProcess',
+    #     seqId: '',
+    #     workitemid: '',
+    #     process: proc_id
+    #   },
+    #   body: data
+    # })
+
   end
 end
 
-r = Reporter.new
-r.login(ENV['USERNAME'], ENV['PASSWORD'])
-r.submit
-puts r.cookies.to_a
+5.times do
+  begin
+    r = Reporter.new
+    r.login(ENV['USERNAME'], ENV['PASSWORD'])
+    r.submit
+    break
+  rescue StandardError
+    next
+  end
+end
